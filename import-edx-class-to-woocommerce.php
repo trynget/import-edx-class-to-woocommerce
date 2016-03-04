d438777066ad9d7c453f0c3888876d5d2f41fa29
Kevin <zkn0109@gmail.com>
2016-03-04 20:35:24 +0800

    Initial commit

---
+++ import-edx-class-to-woocommerce.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: import-edx-class-to-woocommerce
+Plugin URI:
+Description: Import edx classes into you WooCommerce as products.
+Version: 0.1
+Author:
+Author URI:
+License: GPL2
+*/
